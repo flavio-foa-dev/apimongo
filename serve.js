3fc970d145c6194b81ecf91734f30dacd8f7f3c2
Flavio Andrade <foa12381@gmail.com>
2023-08-01 11:08:52 -0300

    feat/config initial

---
+++ serve.js
@@ -0,0 +1,18 @@
+const http = require("http")
+const PORT = 4000
+
+const routes = {
+  "/": "Curso de node",
+  "/books": "listagem de livros",
+  "/authors": "listagem de autores",
+  "/store": "listagem de Products comprados"
+
+}
+
+
+const serve = http.createServer(function(req, res) {
+  res.writeHead(200, {'content-type': 'text/plain'})
+  res.end(routes[req.url])
+})
+
+serve.listen(PORT, ()=> console.log('listening on port', PORT))
